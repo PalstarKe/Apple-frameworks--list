fdf2f60ed0f0dcc761fc414a34c2e79496f2cf8e
Palstar_Ke <maishwriter2@gmail.com>
2024-01-25 06:07:20 +0300

    Refactored the FrameworkTitleView

--- Apple-frameworks/FrameworkGridView.swift
+++ Apple-frameworks/FrameworkGridView.swift
@@ -15,7 +15,7 @@ struct FrameworkGridView: View {
     var body: some View {
         LazyVGrid(columns: columns) {
             ForEach(MockData.frameworks){ framework in
-                FrameworkTitleView(name: framework.name, imageName: framework.imageName)
+                FrameworkTitleView(framework: framework)
             }
         }
     }
@@ -26,15 +26,14 @@ struct FrameworkGridView: View {
 }
 
 struct FrameworkTitleView: View {
-    let name: String
-    let imageName: String
+    let framework: Framework
     
     var body: some View{
         VStack{
-            Image(imageName)
+            Image(framework.imageName)
                 .resizable()
                 .frame(width: 90, height: 90)
-            Text(name)
+            Text(framework.name)
                 .font(.title2)
                 .fontWeight(.semibold)
                 .scaledToFit()
