8e2ab8f195e0e03241a4a128b11c9acf338cc8e8
Palstar_Ke <maishwriter2@gmail.com>
2024-01-25 07:00:06 +0300

    Added the FrameworkDetailView Page

--- Apple-frameworks/FrameworkDetailView.swift
+++ Apple-frameworks/FrameworkDetailView.swift
@@ -2,7 +2,39 @@
 //  FrameworkDetailView.swift
 //  Apple-frameworks
 //
-//  Created by kejos on 25/01/2024.
+//  Created by `Codevibe` on 25/01/2024.
 //
 
-import Foundation
+import SwiftUI
+
+
+struct FrameworkDetailView: View {
+    let framework: Framework
+    
+    var body: some View {
+        VStack{
+            Spacer()
+            FrameworkTitleView(framework: framework)
+            Text(framework.description)
+                .font(.body)
+                .padding()
+            Spacer()
+            Button{
+                
+            } label: {
+               Text("Learn More")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .frame(width: 280, height: 50)
+                    .background(Color.red)
+                    .foregroundStyle(Color.white)
+                    .cornerRadius(10)
+            }
+        }
+    }
+}
+
+#Preview {
+    FrameworkDetailView(framework: MockData.sampleFramework)
+//        .preferredColorScheme(.dark)
+}
