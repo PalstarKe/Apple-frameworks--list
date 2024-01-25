58cd6d6d7ab6bb6c669855e8a7e18232bba0e3cb
Palstar_Ke <maishwriter2@gmail.com>
2024-01-25 04:06:42 +0300

    Initial Commit

---
+++ Apple-frameworks/Apple_frameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_frameworksApp.swift
+//  Apple-frameworks
+//
+//  Created by kejos on 25/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Apple_frameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
